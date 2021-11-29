1083b2c3d07ee834c735c00573387c0ffeb37033
richie9624 <89420436+Richie-9624@users.noreply.github.com>
2021-11-29 19:44:43 +0000

    commit

--- Midterm/midterm2.cpp
+++ Midterm/midterm2.cpp
@@ -68,7 +68,40 @@ class StudentCompare //studentcompare class
 
 int main()
 {
+  priority_queue<Student, vector<Student>, StudentCompare> pq; 
+  //creayes priority queue of students
+
+  //create the scores array for student 1-5
+  double *scores1 = new double[5]{72.5, 38.6, 22.8, 34.5, 55.7};
+  Student s1(1, "Akon", scores1, 5);
   
+  double *scores2 = new double[5]{82.5, 42.7, 85.8, 64, 75};
+  Student s2(2, "Evan", scores2, 5);
+  
+  double *scores3 = new double[5]{80, 48, 45.8, 55, 44.5};
+  Student s3(3, "John", scores3, 5);
+  
+  double *scores4 = new double[5]{51.5, 92.5, 83, 48.5, 66.7};
+  Student s4(4, "Arya", scores4, 5);
+  
+  double *scores5 = new double[5]{57, 98.6, 42.8, 100, 95.2};
+  Student s5(5, "Sans", scores5, 5);
+ 
+  pq.push(s1);
+  pq.push(s2);
+  pq.push(s3);
+  pq.push(s4); //push the students to the priority queue
+  pq.push(s5);
+
+  //pop the items out of the pq and print the info
+  for (int i = 0; i < 5; i++)
+  {
+    Student s = pq.top();
+    s.display(); //display the details of student at top of pq
+    pq.pop();    //pop the student
+    cout << endl;
+  }
+  return 0;
 }
 
 
