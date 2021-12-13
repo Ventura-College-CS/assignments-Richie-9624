6502ef4d95844c5fdb06533b5d15ee07a580a7fb
richie9624 <89420436+Richie-9624@users.noreply.github.com>
2021-12-13 07:42:11 +0000

    added if statements to make sure singe digit and if the correct characters were entered

--- Final3.cpp
+++ Final3.cpp
@@ -9,3 +9,20 @@ float input(char ch) //uses to return float from user input
   return 0;
 }
 
+int Operand(char ch)
+{
+  if(ch >= '0' && ch <= '9') //single digit number
+  {
+    return 1;  //if character is an operand
+  }
+  return -1; //if character isn't an operand
+}
+
+int Operatrr(char ch)  
+{
+  if(ch == '+' || ch == '-' || ch == '*' || ch == '^' || ch == '/')
+  {
+    return 1;
+  }
+  return -1;
+}
