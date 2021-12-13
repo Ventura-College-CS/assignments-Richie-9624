e35aaf59e3900991edd4745994bfa7a799d4c348
richie9624 <89420436+Richie-9624@users.noreply.github.com>
2021-12-13 10:04:29 +0000

    created a way to swap and sort the array

--- final1.cpp
+++ final1.cpp
@@ -17,10 +17,30 @@ class Course
     }
 };
 
-// void swap(int *a, int *b)
-// {
-//   int temp = *a;
-//   *a = *b;
-//   *b = temp;
-// }
+void swap(int *a, int *b)
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void sort(Course arr[10])
+{
+  for(int i = 0; i < 10; i++ )
+  {
+    for (int j = 0; j < 10-i-1; i++)
+    {
+      if(arr[j].id > arr[j+1].id)
+      {
+        swap(&arr[j].id, &arr[j+1].id);
+        string temp = arr[j].name;
+        arr[j].name = arr[j+1].name;
+        arr[j+1].name = temp;
+        swap(&arr[j].credit, &arr[j+1].credit);
+      }
+    }
+  }
+}
+
+
 
