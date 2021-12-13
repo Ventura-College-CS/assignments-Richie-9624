5a6e9e37345534f076187f02c7ba383f9c2e81cb
richie9624 <89420436+Richie-9624@users.noreply.github.com>
2021-12-13 10:31:34 +0000

    created main to find course using the id

--- final1.cpp
+++ final1.cpp
@@ -63,3 +63,14 @@ string BinarySearch(Course arr[10], int id, int start, int end)
   return "not found";
 }
 
+int main()
+{
+  Course c[10] = {Course(10001, "Calculus", 3), Course(10002, "History", 4), Course(10003, "Algebra", 5), Course(10004, "Geometry", 1), Course(10005, "Woodshop", 2), Course(10006, "Metalshop", 1), Course(10007, "Chemistry", 5), Course(10008, "Music", 5), Course(10009, "Theatre", 3), Course(10010, "Communication", 1)};
+
+  int id;
+  cout << "enter course id: ";
+  cin >> id;
+  sort(c);
+  cout << BinarySearch(c, id, 0, 10);
+  return 0;
+}
