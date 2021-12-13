40652a420744568d8358adb6f27a60ed76d497b1
richie9624 <89420436+Richie-9624@users.noreply.github.com>
2021-12-13 16:05:48 +0000

    Created a way for it to read off of a text file

--- final1.cpp
+++ final1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -8,6 +9,8 @@ class Course
     int id;
     string name;
     int credit;
+
+    Course(){}
 
     Course(int ID, string sname, int coursecredit)
     {
@@ -65,12 +68,28 @@ string BinarySearch(Course arr[10], int id, int start, int end)
 
 int main()
 {
-  Course c[10] = {Course(10001, "Calculus", 3), Course(10002, "History", 4), Course(10003, "Algebra", 5), Course(10004, "Geometry", 1), Course(10005, "Woodshop", 2), Course(10006, "Metalshop", 1), Course(10007, "Chemistry", 5), Course(10008, "Music", 5), Course(10009, "Theatre", 3), Course(10010, "Communication", 1)};
-
+  Course c[10];
+  int i = 0;
   int id;
-  cout << "enter course id: ";
+
+  ifstream ifs ("final.txt");
+  while (ifs >> c[i].id)
+  {
+    ifs >> c[i].name >> c[i].credit;
+    i++;
+  }
+  ifs.close();
+
+  cout << "enter course ID: ";
   cin >> id;
-  sort(c);
-  cout << BinarySearch(c, id, 0, 10);
+  int pos = BinarySearch(c, id, 0, 10);
+  if(pos == -1)
+  {
+    cout << "not found" << endl;
+  }
+  else
+  {
+    cout << "course id: " << c[pos].id << " Name: " << c[pos].name << " Credits: " << c[pos].credit << endl;
+  }
   return 0;
 }
